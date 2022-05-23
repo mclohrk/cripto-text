1f8a595aaacd511c717239be9038bba11670eac2
mclohrk <marcelo.henrik@gmail.com>
2022-05-23 10:59:49 -0300

    Up inicial

---
+++ README.md
@@ -0,0 +1,28 @@
+# Criptografia de Mensagens
+
+Projeto de criptografia de texto. Programa para criptografia de mensagens. Utilize o espaço para escrever sua mensagem e o botão "criptografar " para esconder a mensagem, com o botão "copiar " você pode selecionar e copiar a mensagem cifrada. Utilize sempre letras minúsculas sem caracteres
+                    especiais.
+
+## 🚀 ⚙️ 📦 Caracteristicas
+
+Neste projeto é possível criptografar mensagens com regras pessoais que podem ser modificadas, criadas e adicionadas de forma fácil e simples.
+
+## 🛠️ Construído com
+
+Projeto Web construido em JavaScript + CSS3 + HTML5 e Bootsrap
+
+
+* [Bootsrap](https://getbootstrap.com/) - Template basico bootstrap
+* [html5up](https://html5up.net/) - Gerente de Dependência
+
+
+
+## 📄 Licença
+
+Este projeto está sob a licença (sua licença) - veja o arquivo [LICENSE.md](https://github.com/usuario/projeto/licenca) para detalhes.
+
+
+## ✒️ Autor
+
+* **I am** *
+⌨️ com ❤️ por [mclohrk](https://github.com/mclohrk)
---
+++ js/criptorius.js
@@ -0,0 +1,134 @@
+function encriptar(data) {
+    /* 
+    A letra "a" é convertida para "ai"
+    A letra "e" é convertida para "enter"
+    A letra "i" é convertida para "imes"
+    A letra "o" é convertida para "ober"
+    A letra "u" é convertida para "ufat"
+    */
+    // var data = document.getElementById("txtarea_enc");
+
+    var tam = data.length;
+    var textEncriptado = [];
+
+    for (i = 0; i < tam; i++) {
+        const char_ = data[i].charAt().toLowerCase()
+        switch (char_) {
+            case 'a':
+                textEncriptado[i] = "ai";
+                break;
+            case 'e':
+                textEncriptado[i] = "enter";
+                break;
+            case 'i':
+                textEncriptado[i] = "imes";
+                break;
+            case 'o':
+                textEncriptado[i] = "ober";
+                break;
+            case 'u':
+                textEncriptado[i] = "ufat";
+                break;
+            default:
+                textEncriptado[i] = data[i];
+
+        }
+
+    }
+    return textEncriptado.join("");
+}
+
+function desencriptar(data) {
+    /* 
+    A letra "a" é convertida para "ai"
+    A letra "e" é convertida para "enter"
+    A letra "i" é convertida para "imes"
+    A letra "o" é convertida para "ober"
+    A letra "u" é convertida para "ufat"
+    */
+    var textDesencriptado = data;
+    textDesencriptado = data.replace(/ai/g, "a");
+    textDesencriptado = textDesencriptado.replace(/enter/g, "e");
+    textDesencriptado = textDesencriptado.replace(/imes/g, "i");
+    textDesencriptado = textDesencriptado.replace(/ober/g, "o");
+    textDesencriptado = textDesencriptado.replace(/ufat/g, "u");
+    return textDesencriptado;
+
+
+}
+
+function limpaText() {
+    document.getElementById('txtarea_enc').value = " ";
+    //  document.getElementById('txta').value = " ";
+}
+
+function copyTextArea() {
+    //printMensagem(data)
+    let textoCopiado = document.getElementById("txtarea_enc");
+    textoCopiado.select();
+    textoCopiado.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    //  alert("O texto é: " + textoCopiado.value);
+    //return data;
+    alert(" Texto Copiado ");
+}
+
+//retira acentos de letras/palavras
+function retiraAcentos(string) {
+
+    var stringModified = string.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    return stringModified;
+
+}
+
+//retira caracter special
+function retirarCaracterSpecial(data) {
+    return desired = data.replace(/[^\w\s]/gi, '');
+}
+
+function exibirMensagem(data) {
+    data = document.getElementById("txtarea_enc");
+    var ddata = document.getElementById("txta");
+    var dataLowerCase = data.value.toLowerCase();
+    var result = (encriptar(retiraAcentos(dataLowerCase)));
+    ddata.innerHTML = result;
+    return dataLowerCase;
+}
+
+function exibirMensagem_desencript(data) {
+    data = document.getElementById("txtarea_enc");
+    var ddata = document.getElementById("txta");
+    var dataLowerCase = data.value.toLowerCase();
+    //printMensagem(desencriptar(retiraAcentos(dataLowerCase)));
+    // printMensagem(desencriptar(dataLowerCase));
+    var desencripts = desencriptar(dataLowerCase);
+    ddata.innerHTML = desencripts;
+    return desencripts;
+}
+
+function toLowerMensagem(data) {
+    var dataLowerCase = data.value.toLowerCase()
+    return dataLowerCase;
+}
+
+function printMensagem(data) {
+    alert(data);
+}
+//aborta caso texto possua letra maiuscula
+function exibirMensagem_alert() {
+    var data = document.getElementById("txtarea_enc");
+    if (data.value === data.value.toLowerCase()) {
+        printMensagem(data.value);
+
+    } else {
+        document.write("Digite apenas letras minúsculas")
+    }
+}
+var btn = document.getElementById("btn");
+btn.addEventListener("click", exibirMensagem);
+
+var btn = document.getElementById("btn_desencript");
+btn.addEventListener("click", exibirMensagem_desencript);
+
+var btn = document.getElementById("btn_copy");
+btn.addEventListener("click", copyTextArea);
